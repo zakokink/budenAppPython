b6d49a67afe2f62617684927a9d9de1c325f5b27
zakokink <salomon.zilberberg@sti-consulting.com>
2024-10-28 10:49:36 +0100

    Modelle implementiert

--- budenApp/myBudenApp/admin.py
+++ budenApp/myBudenApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, Uebungen, Training
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Uebungen)
+admin.site.register(Training)
--- budenApp/myBudenApp/models.py
+++ budenApp/myBudenApp/models.py
@@ -1,3 +1,26 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+
+class Uebungen(models.Model):
+    uebung = models.CharField(max_length=100)
+    def __str__(self):
+        return self.uebung
+
+class Training(models.Model):
+    date = models.DateField(default=datetime.now)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    uebung = models.ForeignKey(Uebungen, on_delete=models.PROTECT)
+    gewicht = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    wiederholungen = models.CharField(max_length=200)
+    comment = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.date)  + ' ' + self.user.name + ' ' + str(self.uebung)
+
